8692c187eb311915baae56326e81d5f66fb6c4c2
Герман Курочкин <ggkurochkin@edu.hse.ru>
2020-06-02 12:14:56 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace practica3
+{
+    class Program
+    {
+        static double InputNum()
+        {
+            double num;
+            string input;
+            bool ok;
+            do
+            {
+                input = Console.ReadLine();
+                ok = double.TryParse(input, out num);
+                if (!ok) Console.WriteLine("Некорректный ввод");
+
+            } while (!ok);
+
+            return num;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = InputNum();
+            Console.WriteLine("Введите y");
+            double y = InputNum();
+            if ((x >= 0&&(x*x+y*y<=1))||(x <= 0&&y>=-0.5*x-1&&y<=0.5*x+1)) 
+            { 
+                Console.WriteLine("Принадлежит"); 
+            }
+            else Console.WriteLine("Не принадлежит");
+        }
+    }
+}
